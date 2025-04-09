70c5319faae78287028219d23b5030ec7d38a962
Maël Gruand <maelgruand1@yahoo.com>
2025-04-09 11:56:32 +0200

    Creo el login

---
+++ src/components/User.ts
@@ -0,0 +1,15 @@
+class Users{
+    private nameUser : string;
+    private passwordUser : string
+
+    constructor(userName : string, pswd : string){
+        this.nameUser = userName;
+        this.passwordUser = pswd;
+    }
+
+    
+
+}
+
+
+export default Users;
