74038669fd4bd55cf9670338eddb5c0c4e87d828
maelgruand1 <maelgruand933@gmail.com>
2025-04-09 16:42:55 +0200

    Revert "Corrigo un error accidental de borro App.tsx"
    This reverts commit 84c0e8e21a5e254570a0b77ad9ce1c72527de094.

---
+++ src/components/modules/User.ts
@@ -0,0 +1,12 @@
+class Users{
+    private nameUser : string;
+    private passwordUser : string
+
+    constructor(userName : string, pswd : string){
+        this.nameUser = userName;
+        this.passwordUser = pswd;
+    }
+}
+
+
+export default Users;
